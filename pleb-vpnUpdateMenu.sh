0f848ddd3f3c8ad96a41747853cab3e6f1920b8a
allyourbankarebelongtous <100060902+allyourbankarebelongtous@users.noreply.github.com>
2023-01-23 10:10:18 -0600

    Update pleb-vpnUpdateMenu.sh

--- pleb-vpnUpdateMenu.sh
+++ pleb-vpnUpdateMenu.sh
@@ -5,14 +5,6 @@
 plebVPNConf="/home/admin/pleb-vpn/pleb-vpn.conf"
 source ${plebVPNConf}
 
-# Cancel check function
-function cancel_check(){
-  if [[ -z "$1" ]]; then
-    echo "Cancelled"
-    /home/admin/app-data/pleb-vpn/pleb-vpnMenu.sh
-  fi
-}
-
 # BASIC MENU INFO
 WIDTH=66
 BACKTITLE="Update/Uninstall"
@@ -47,16 +39,9 @@ associated services from your system?
 This cannot be undone, and you would have to redo all of 
 your configurations and certs if you re-install at a later time.
       " 12 70
-    if [ $? -eq 0 ]; then
-      exit 0
-    else
+    if [ $? -eq 1 ]; then
       /home/admin/pleb-vpn/pleb-vpn.install.sh uninstall
-      /home/admin/00mainMenu.sh
+      exit 0
     fi
     ;;
 esac
-
-exitCode=$?
-if [ "${exitCode}" = "0" ]; then
-  /home/admin/pleb-vpn/pleb-vpnMenu.sh
-fi
