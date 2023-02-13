6ab39f53a3b71a8c3202eccaf6dee7e4051723b8
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-02-12 20:36:40 -0600

    Update managepayments.sh

--- payments/managepayments.sh
+++ payments/managepayments.sh
@@ -69,7 +69,7 @@ ${FREQ} PAYMENTS" >>/home/admin/pleb-vpn/payments/displaypayments.tmp
       node_id=$(cat $(echo "${currentPayments}" | sed -n "${inc1}p") | awk '{print $6}' | cut -c 1-20)
       value=$(cat $(echo "${currentPayments}" | sed -n "${inc1}p") | awk '{print $4 $3}')
       message=$(cat $(echo "${currentPayments}" | sed -n "${inc1}p") | sed 's/.*message//' | sed 's/ //' | sed 's/\"//g')
-      echo "${short_node_id}_${freq}_${node}     $node_id     ${value}          ${message}" >>/home/admin/pleb-vpn/payments/displaypayments.tmp
+      echo -e "${short_node_id}_${freq}_${node} \t$node_id \t${value} \t\t${message}" >>/home/admin/pleb-vpn/payments/displaypayments.tmp
       echo "PAYMENTS+=(${short_node_id}_${freq}_${node}" >>/home/admin/pleb-vpn/payments/selectpayments.tmp
       sudo sed -i "s/${short_node_id}_${freq}_${node}.*/${short_node_id}_${freq}_${node} \"send to ${short_node_id} ${value} ${freq} from ${node}\"\)/g" /home/admin/pleb-vpn/payments/selectpayments.tmp
       ((inc1++))
