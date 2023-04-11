af855879b1e05311fcd1fc11ad69381bd0562e38
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-10 19:22:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from webui import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port = 2420, debug=True)
 
 else:
     gunicorn_app = create_app()
