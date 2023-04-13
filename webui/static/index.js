775cc10e81654693751608ee198c5fab629e28d0
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-12 23:31:34 -0500

    Add vpn.install.sh
    Add vpn install script and webui components

--- webui/static/index.js
+++ webui/static/index.js
@@ -1,10 +1,33 @@
+function delete_plebvpn_conf(userId) {
+  if (
+    confirm_dialog(
+      (message =
+        "Are you sure you want to delete your uploaded plebvpn.conf? You will have to upload a new plebvpn.conf file to enable Pleb-VPN.")
+    ) == true
+  ) {
+    fetch("/delete_plebvpn_conf", {
+      method: "POST",
+      body: JSON.stringify({ userId: userId }),
+    }).then((_res) => {
+      window.location.href = "/pleb-VPN";
+    });
+  }
+}
+
 function setplebVPN(userId) {
-  fetch("/set_plebVPN", {
-    method: "POST",
-    body: JSON.stringify({ userId: userId }),
-  }).then((_res) => {
-    window.location.href = "/pleb-VPN";
-  });
+  if (
+    confirm_dialog(
+      (message =
+        "Use currently uploaded plebvpn.conf file and turn on Pleb-VPN?")
+    ) == true
+  ) {
+    fetch("/set_plebVPN", {
+      method: "POST",
+      body: JSON.stringify({ userId: userId }),
+    }).then((_res) => {
+      window.location.href = "/pleb-VPN";
+    });
+  }
 }
 
 function refreshplebVPNdata(userId) {
@@ -14,4 +37,9 @@ function refreshplebVPNdata(userId) {
   }).then((_res) => {
     window.location.href = "/";
   });
+}
+
+function confirm_dialog(message) {
+  var result = confirm(message);
+  return result;
 }
--- webui/views.py
+++ webui/views.py
@@ -8,7 +8,7 @@ import json, os, subprocess
 views = Blueprint('views', __name__)
 
 ALLOWED_EXTENSIONS = {'conf'}
-UPLOAD_FOLDER = '/mnt/hdd/mynode/pleb-vpn/'
+PLEBVPN_CONF_UPLOAD_FOLDER = '/mnt/hdd/mynode/pleb-vpn/openvpn'
 conf_file_location = '/mnt/hdd/mynode/pleb-vpn/pleb-vpn.conf'
 plebVPN_status = {}
 
@@ -20,39 +20,39 @@ def home():
 @views.route('/refresh_plebVPN_data', methods=['POST'])
 @login_required
 def refresh_plebVPN_data():
+    # refresh pleb-vpn status of connection to vps
     global plebVPN_status
-    # generate new .tmp file
-    # for testing purposes, rename pleb-vpn_status1.tmp
-    os.rename(os.path.abspath('./pleb-vpn_status1.tmp'), os.path.abspath('./pleb-vpn_status.tmp'))
     plebVPN_status = get_plebVPN_status()
-    # delete pleb-vpn_status.tmp file
-    os.rename(os.path.abspath('./pleb-vpn_status.tmp'), os.path.abspath('./pleb-vpn_status1.tmp'))
 
     return jsonify({})
 
 @views.route('/pleb-VPN', methods=['GET', 'POST'])
 @login_required
 def pleb_VPN():
+    # upload plebvpn.conf file
     if request.method == 'POST':
         # check if the post request has the file part
         if 'plebvpn_conf' not in request.files:
             flash('No file part', category='error')
             return redirect(request.url)
         file = request.files['plebvpn_conf']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        # If the user does not select a file, the browser submits an empty file without a filename.
         if file.filename == '':
             flash('No selected file', category='error')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            # make directory if it doesn't exist
+            if not os.path.exists(PLEBVPN_CONF_UPLOAD_FOLDER):
+                os.mkdir(PLEBVPN_CONF_UPLOAD_FOLDER)
+            file.save(os.path.join(PLEBVPN_CONF_UPLOAD_FOLDER, filename))
             flash('Upload successful!', category='success')
 
     return render_template("pleb-vpn.html", user=current_user, setting=get_conf(), plebVPN_status=plebVPN_status)
 
 @views.route('/set_plebVPN', methods=['POST'])
 def set_plebVPN():
+    # turns pleb-vpn connection to vps on or off
     setting = get_conf()
     user = json.loads(request.data)
     userId = user['userId']
@@ -60,11 +60,33 @@ def set_plebVPN():
     if user:
         if user.id == current_user.id:
             if setting['plebVPN'] == 'on':
-                set_conf('plebVPN', 'off')
-                flash('Pleb-VPN disconnected', category='success')
+                cmd_str = ["sudo", "/mnt/hdd/mynode/pleb-vpn/vpn.install.sh", "off"]
+                result = subprocess.run(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if not result.stderr:
+                    flash('Pleb-VPN disconnected.', category='success')
+                else:
+                    flash('An unknown error occured!', category='error')
             else:
-                set_conf('plebVPN', 'on')
-                flash('Pleb-VPN connected!', category='success')
+                cmd_str = ["sudo", "/mnt/hdd/mynode/pleb-vpn/vpn.install.sh", "on"]
+                result = subprocess.run(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if not result.stderr:
+                    flash('Pleb-VPN connected!', category='success')
+                else:
+                    flash('An unknown error occured!', category='error')
+    
+    return jsonify({})
+
+@views.route('/delete_plebvpn_conf', methods=['POST'])
+def delete_plebvpn_conf():
+    # delete plebvpn.conf from pleb-vpn/openvpn
+    user = json.loads(request.data)
+    userId = user['userId']
+    user = User.query.get(userId)
+    if user:
+        if user.id == current_user.id:
+            if os.path.exists(os.path.abspath('./openvpn/plebvpn.conf')):
+                os.remove(os.path.abspath('./openvpn/plebvpn.conf'))
+                flash('plebvpn.conf file deleted', category='success')
     
     return jsonify({})
 
@@ -72,10 +94,10 @@ def set_conf(name, value):
     setting = get_conf()
     if not setting[name]:
         cmd_str = ["sed", "-i", "2i" + name + "=", conf_file_location]
-        subprocess.run(cmd_str, shell=True)
-    cmd_str = [sed_exe, "-i", "s:^" + name + "=.*:" + name + "=" + value + ":g", conf_file_location]
+        subprocess.run(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cmd_str = ["sed", "-i", "s:^" + name + "=.*:" + name + "=" + value + ":g", conf_file_location]
     print(cmd_str)
-    subprocess.run(cmd_str, shell=True)
+    subprocess.run(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def get_conf():
     setting = {}
@@ -87,13 +109,16 @@ def get_conf():
     return setting
 
 def get_plebVPN_status():
+    # get status of pleb-vpn connection to vps
     plebVPN_status = {}
-    if os.path.exists(os.path.abspath('./pleb-vpn_status.tmp')):
-        with open(os.path.abspath('./pleb-vpn_status.tmp')) as status:
-            for line in status:
-                if "=" in line:
-                    name, value = line.split("=")
-                    plebVPN_status[name] = str(value).rstrip().strip('\'\'')
+    cmd_str = ["sudo", "/mnt/hdd/mynode/pleb-vpn/vpn.install.sh", "status"]
+    subprocess.run(cmd_str, shell=True)
+    with open(os.path.abspath('./pleb-vpn_status.tmp')) as status:
+        for line in status:
+            if "=" in line:
+                name, value = line.split("=")
+                plebVPN_status[name] = str(value).rstrip().strip('\'\'')
+    os.remove(os.path.abspath('./pleb-vpn_status.tmp'))
     return plebVPN_status
 
 def allowed_file(filename):
