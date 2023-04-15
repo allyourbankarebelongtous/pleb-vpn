8ea75c331659bf049d4f533801e72901d9148c17
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-15 14:27:53 -0500

    Update views.py

--- webui/views.py
+++ webui/views.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, request, flash, jsonify, request, redirect, url_for
 from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
-from socketio import socketio
 from .models import User
 from . import db
-import json, os, subprocess, keyboard
+import json, os, subprocess, keyboard, socketio
 
 views = Blueprint('views', __name__)
 
