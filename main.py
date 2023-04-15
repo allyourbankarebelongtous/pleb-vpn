363e86d8e9e58979523a5d54ba4fdc145d55c0ed
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-15 14:37:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from webui import create_app
 from socket_io import socketio
 
 app = create_app()
+socketio.init_app(app)
 
 if __name__ == '__main__':
     socketio.run(app, host = "0.0.0.0", port = 2420, debug=True)
