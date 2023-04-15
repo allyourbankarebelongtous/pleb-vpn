819da564288875ab25eb232eba9a5cd7ec000acc
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-15 13:46:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = create_app()
 socketio = SocketIO(app)
 
 if __name__ == '__main__':
-    socketio.run(host = "0.0.0.0", port = 2420, debug=True)
+    socketio.run(app, host = "0.0.0.0", port = 2420, debug=True)
 
 else:
     gunicorn_app = create_app()
