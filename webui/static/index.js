7b10f050f501d60dd4e2c5b0cc69462077af3357
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-20 23:48:33 -0500

    bug fix

--- webui/static/index.js
+++ webui/static/index.js
@@ -15,51 +15,46 @@ function delete_plebvpn_conf(userId) {
 }
 
 function setplebVPN_on(userId, event) {
-  event.preventDefault(); // Prevent the default form submission behavior
   if (
     confirm_dialog(
       (message =
         "Use currently uploaded plebvpn.conf file and turn on Pleb-VPN?")
     ) == true
   ) {
-    document.getElementById("activate").style.display = "none";
-    document.getElementById("activate_loading").style.display = "inline-block";
-    document.getElementById("deactivate").style.display = "none";
-    document.getElementById("deactivate_loading").style.display =
-      "inline-block";
+    document.getElementById("activate").classList.add("d-none");
+    document.getElementById("activate_loading").classList.remove("d-none");
+    document.getElementById("deactivate").classList.add("d-none");
+    document.getElementById("deactivate_loading").classList.remove("d-none");
     fetch("/set_plebVPN", {
       method: "POST",
       body: JSON.stringify({ userId: userId }),
     }).then((_res) => {
-      document.getElementById("activate").style.display = "inline-block";
-      document.getElementById("activate_loading").style.display = "none";
-      document.getElementById("deactivate").style.display = "inline-block";
-      document.getElementById("deactivate_loading").style.display = "none";
+      document.getElementById("activate").classList.remove("d-none");
+      document.getElementById("activate_loading").classList.add("d-none");
+      document.getElementById("deactivate").classList.remove("d-none");
+      document.getElementById("deactivate_loading").classList.add("d-none");
       window.location.href = "/pleb-VPN";
     });
   }
 }
 
 function setplebVPN_off(userId, event) {
-  event.preventDefault(); // Prevent the default form submission behavior
   if (
     confirm_dialog((message = "Are you sure you want to turn off Pleb-VPN?")) ==
     true
   ) {
-    document.getElementById("activate").style.display = "none";
-    document.getElementById("activate_loading").style.display = "inline-block";
-    document.getElementById("deactivate").style.display = "none";
-    document.getElementById("deactivate_loading").style.display = fetch(
-      "/set_plebVPN",
-      {
-        method: "POST",
-        body: JSON.stringify({ userId: userId }),
-      }
-    ).then((_res) => {
-      document.getElementById("activate").style.display = "inline-block";
-      document.getElementById("activate_loading").style.display = "none";
-      document.getElementById("deactivate").style.display = "inline-block";
-      document.getElementById("deactivate_loading").style.display = "none";
+    document.getElementById("activate").classList.add("d-none");
+    document.getElementById("activate_loading").classList.remove("d-none");
+    document.getElementById("deactivate").classList.add("d-none");
+    document.getElementById("deactivate_loading").classList.remove("d-none");
+    fetch("/set_plebVPN", {
+      method: "POST",
+      body: JSON.stringify({ userId: userId }),
+    }).then((_res) => {
+      document.getElementById("activate").classList.remove("d-none");
+      document.getElementById("activate_loading").classList.add("d-none");
+      document.getElementById("deactivate").classList.remove("d-none");
+      document.getElementById("deactivate_loading").classList.add("d-none");
       window.location.href = "/pleb-VPN";
     });
   }
