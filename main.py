64e6d1d2d3cc791adfc7f29e2e45607739718d6d
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-04-23 18:02:35 -0500

    bug fix

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from webui import create_app
 from socket_io import socketio
+from gevent import monkey
 
 app = create_app()
 socketio.init_app(app)
 
 if __name__ == '__main__':
+    
+    monkey.patch_all()
     socketio.run(app, host = "0.0.0.0", port = 2420, debug=True)
-
-else:
-    gunicorn_app = create_app()
--- socket_io.py
+++ socket_io.py
@@ -1,6 +1,3 @@
 from flask_socketio import SocketIO
-from gevent import monkey
-
-monkey.patch_all()
 
 socketio = SocketIO(async_mode='gevent', websocket=True)
