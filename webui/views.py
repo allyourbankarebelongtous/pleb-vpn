01f5778f6cb3fa64250cec82cd0d8afd4c14414b
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-05-26 11:21:03 -0500

    Update views.py

--- webui/views.py
+++ webui/views.py
@@ -981,19 +981,24 @@ def set_enter_input():
 
 # check date of last commit to https://github.com/allyourbankarebelongtous/pleb-vpn/
 def check_repository_updated(branch=""):
-    # Construct the API endpoint URL
     url = f"https://api.github.com/repos/allyourbankarebelongtous/pleb-vpn/commits/{branch}"
+    print("url =", url)
     try:
-        # Send a GET request to the API
         response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if the request fails
-        # Get the date of the last commit
-        last_commit_date = response.json()[0]["commit"]["committer"]["date"]
-        # Format the date in the same format as git log -1 --format=%ci
-        dt = datetime.strptime(last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
-        formatted_date = dt.strftime("%Y-%m-%d %H:%M:%S %z")
-        
-        return formatted_date
+        response.raise_for_status()
+        data = response.json()
+        print("json response")
+        print(data)
+
+        if data:  # Check if data is not empty
+            last_commit_date = data["commit"]["committer"]["date"]
+            dt = datetime.strptime(last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
+            formatted_date = dt.strftime("%Y-%m-%d %H:%M:%S %z")
+            print("formatted_date=", formatted_date)
+            return formatted_date
+        else:
+            print("no data")
+            return None
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return None
