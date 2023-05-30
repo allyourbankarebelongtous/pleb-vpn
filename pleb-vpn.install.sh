ededf12d71264697d14f162902a996e2d1b9db73
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-05-30 00:22:06 -0500

    Update pleb-vpn.install.sh

--- pleb-vpn.install.sh
+++ pleb-vpn.install.sh
@@ -60,7 +60,7 @@ on() {
     sudo mkdir pleb-vpn-tmp
     cd pleb-vpn-tmp
     sudo wget https://github.com/allyourbankarebelongtous/pleb-vpn/archive/refs/tags/${ver}.tar.gz
-    sudo tar -xzf ${ver}.tar.gz
+    sudo tar -xzf "${ver}".tar.gz
     isSuccess=$(ls /home/admin/pleb-vpn-tmp/ | grep -c pleb-vpn)
     if [ ${isSuccess} -eq 0 ]; then
       echo "error: download and unzip failed. Check internet connection and version number and try again."
@@ -77,7 +77,7 @@ on() {
         sudo mkdir pleb-vpn-tmp
         cd pleb-vpn-tmp
         sudo wget https://github.com/allyourbankarebelongtous/pleb-vpn/archive/refs/tags/${ver}.tar.gz
-        sudo tar -xzf ${ver}.tar.gz
+        sudo tar -xzf "${ver}".tar.gz
         isSuccess=$(ls /home/admin/pleb-vpn-tmp/ | grep -c pleb-vpn)
         if [ ${isSuccess} -eq 0 ]; then
           echo "error: download and unzip failed. Check internet connection and version number and try again."
@@ -401,7 +401,7 @@ update() {
   sudo mkdir /home/admin/pleb-vpn-tmp
   cd /home/admin/pleb-vpn-tmp
   sudo wget https://github.com/allyourbankarebelongtous/pleb-vpn/archive/refs/tags/${latestversion}.tar.gz
-  sudo tar -xzf ${latestversion}.tar.gz
+  sudo tar -xzf "${latestversion}".tar.gz
   isSuccess=$(ls /home/admin/pleb-vpn-tmp/ | grep -c pleb-vpn)
   if [ ${isSuccess} -eq 0 ]; then
     echo "error: download and unzip failed. Check internet connection and version number and try again."
