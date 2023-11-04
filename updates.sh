e7f272130e58c7345a2d65958afdfee11a2b4f5b
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-11-03 23:02:02 -0500

    ver 1.1.0-beta.8

--- updates.sh
+++ updates.sh
@@ -5,7 +5,7 @@
 # make sure updates can be re-run multiple times
 # keep updates present until most users have had the chance to update
 
-ver="v1.1.0-beta.7"
+ver="v1.1.0-beta.8"
 
 # get node info# find home directory based on node implementation
 if [ -d "/mnt/hdd/mynode/pleb-vpn/" ]; then
