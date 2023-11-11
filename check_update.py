6d5128aa8fd42a275557065a0ec7672213acb71b
allyourbankarebelongtous <allyourbankarebelongtous@protonmail.com>
2023-11-11 11:08:16 -0600

    update to version 1.1.1-beta.1
    fixes check_update.py to pull the latest github-defined release instead of parsing version numbers.

--- check_update.py
+++ check_update.py
@@ -1,46 +1,20 @@
 import requests
-import re
 import markdown
 
 def get_latest_version():
-    url = "https://api.github.com/repos/allyourbankarebelongtous/pleb-vpn/releases"
+    url = "https://api.github.com/repos/allyourbankarebelongtous/pleb-vpn/releases/latest"
     response = requests.get(url)
-    
     if response.status_code == 200:
         data = response.json()
-        versions = [release["tag_name"] for release in data]
-        sorted_versions = sorted(versions, key=version_key, reverse=True)
-        latest_version = sorted_versions[0] if sorted_versions else None
-        
-        # Retrieve changelog if a latest version is found
+        latest_version = data.get("tag_name")
+        changelog = data.get("body")
         if latest_version:
-            changelog = get_changelog(latest_version)
             html_changelog = convert_markdown_to_html(changelog)
             return latest_version, html_changelog
         else:
             return None, None
     else:
         return None, None
-
-def version_key(version):
-    match = re.search(r'v(\d+)\.(\d+)', version)
-    if match:
-        major = int(match.group(1))
-        minor = int(match.group(2))
-        return major, minor, version
-    else:
-        return float('inf'), version
-
-def get_changelog(version):
-    url = f"https://api.github.com/repos/allyourbankarebelongtous/pleb-vpn/releases/tags/{version}"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        changelog = data.get("body")
-        return changelog
-    else:
-        return None
 
 def convert_markdown_to_html(markdown_text):
     html = markdown.markdown(markdown_text)
@@ -50,8 +24,6 @@ latest_version, changelog = get_latest_version()
 
 if latest_version:
     print("Latest Version:", latest_version)
-    html_changelog = convert_markdown_to_html(changelog)
-    print("Changelog:\n", html_changelog)
+    print("Changelog:\n", changelog)
 else:
     print("Failed to retrieve the latest version and changelog.")
-
